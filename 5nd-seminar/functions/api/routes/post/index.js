91577cd34506d32f82fba5831dd00ce4737c1187
=Jaeho-Jo <= jojaeho1230@naver.com>
2021-12-23 15:31:41 +0900

    [추가]7주차 실습

--- 5nd-seminar/functions/api/routes/post/index.js
+++ 5nd-seminar/functions/api/routes/post/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
+const{ checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
+const uploadImage =require ('../../../middlewares/uploadImage');
 
 router.get('/list', require('./postListGET'));
 router.post('/', require('./postPOST'));
 
-router.get('/:postId', require('./postGET'));
-router.put('/:postId', require('./postPUT'));
+router.get('/:postId', checkUser, require('./postGET'));
+router.put('/:postId',uploadImage,require('./postPUT'));
 router.delete('/:postId', require('./postDELETE'));
 
 module.exports = router;
